e80b2f5e4ee34fde27be5db3cc0bb45bf2b9568d
Daniel Bates <aftereffector@gmail.com>
2017-12-29 22:11:46 -0500

    Lesson 23: Constants
    From stackoverflow: must define _USE_MATH_DEFINES to expose predefined constants in <math.h> (also seen in comments for math.h using Go To Definition)

--- Udemy - C Programming/main.c
+++ Udemy - C Programming/main.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 int main(int argc, char **argv)
 {
-	double subtotal;
-	double tax;
-	double grandtotal;
-	double taxrate;
-
-	taxrate = 0.175;
-	subtotal = 200;
-	tax = subtotal * taxrate;
-	grandtotal = subtotal + tax;
-
-	printf("The tax on %.2f is %.2f, so the grand total is %.2f.\n", subtotal, tax, grandtotal);
+	#define PI 3.141593
+	printf("%f\n", PI);
+	printf("%.10f\n", M_PI);
 	return 0;
 }
