b30bb55468795b8abe58ca5f807455fb551b2c1b
Daniel Bates <aftereffector@gmail.com>
2017-12-29 22:59:43 -0500

    Lesson 30.1: If and else tests using atoi function

--- Udemy - C Programming/main.c
+++ Udemy - C Programming/main.c
@@ -1,27 +1,13 @@
 #include <stdio.h>
 
-void showPrefixAndPostfixOps() {
-	int num1, num2;
-	printf("\nPrefix and Postfix operators... (num1 = 10)");
-	num1 = 10;
-	num2 = num1++; // num2 = 10, num1 = 11
-	printf("\nnum2 = num1++; so num2 = %d and num1 = %d", num2, num1);
-
-	num1 = 10;
-	num2 = ++num1; // num2 = 11, num1 = 11
-	printf("\nnum2 = ++num1; so num2 = %d and num1 = %d", num2, num1);
-}
-
 int main(int argc, char **argv) {
+	char agestring[10];
 	int age;
 	int bonus;
 
-	int a;
-	int b;
-	a = 10;
-	b = 2;
-
-	age = 70;
+	printf("Enter your age : ");
+	gets(agestring);
+	age = atoi(agestring);	//uses "a to i" function (atoi) to convert to integer
 	if (age > 45) {
 		bonus = 1000;
 	}
@@ -30,45 +16,6 @@ int main(int argc, char **argv) {
 	}
 	printf("Your age is %d, so your bonus is %d.\n", age, bonus);
 
-	if (age <= 70) {
-		printf("You are one of our youngest employees!\n");
-	}
 
-	if (bonus >= 1000) {
-		printf("You've earned a high bonus!\n");
-	}
-
-	printf("\n\nHere are examples of some compound assignment operators...\n");
-	a = a + b;
-	printf("\na + b  : %d\n", a);
-	a = 10;
-	a += b;
-	printf("a += b  : %d\n", a);
-	a = 10;
-	a = a - b;
-	printf("a - b  : %d\n", a);
-	a = 10;
-	a -= b;
-	printf("a -= b  : %d\n", a);
-	a = 10;
-	a = a * b;
-	printf("a * b  : %d\n", a);
-	a = 10;
-	a *= b;
-	printf("a *= b  : %d\n", a);
-	a = 10;
-	a = a / b;
-	printf("a/b  : %d\n", a);
-	a = 10;
-	a /= b;
-	printf("a /= b  : %d\n", a);
-	a = 10;
-	a++;
-	printf("a++ : %d\n", a);
-	a = 10;
-	a--;
-	printf("a-- : %d\n", a);
-
-	showPrefixAndPostfixOps();
 	return(0);
 }
