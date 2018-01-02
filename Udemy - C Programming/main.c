69de46dd212486eda39bcff82150ab0c166b3af6
Daniel Bates <aftereffector@gmail.com>
2018-01-02 15:01:29 -0500

    Lesson 39: Arrays

--- Udemy - C Programming/main.c
+++ Udemy - C Programming/main.c
@@ -1,58 +1,20 @@
 #include <stdio.h>
 
-char *chartype;
-
-void findchartype(int i) {
-	switch(i){
-		case 0:
-			chartype = "NULL";
-			break;
-		case 7:
-			chartype = "Bell";
-			break;
-		case 8:
-			chartype = "Backspace";
-			break;
-		case 9:
-			chartype = "Tab";
-			break;
-		case 10:
-			chartype = "Linefeed";
-			break;
-		case 13:
-			chartype = "Carriage Return";
-			break;
-		case 32:
-			chartype = "Space";
-			break;
-		case 48:
-		case 49:
-		case 50:
-		case 51:
-		case 52:
-		case 53:
-		case 54:
-		case 55:
-		case 56:
-		case 57:
-			chartype = "Number";
-			break;
-		default:
-			chartype = "Character (default)";
-			break;
-	}
-}
-
-void showascii() {
-	int i;
-	char c;
-	for (i = 0; i <= 127; i++) {
-		findchartype(i);
-		printf("%d = %c\t\t[%s]\n", i, i, chartype);
-	}
-}
+int array1[5];
 
 int main(int argc, char **argv) {
-	showascii();
+	int i;
+
+	array1[0] = 11;
+	array1[1] = 22;
+	array1[2] = 33;
+	array1[3] = 44;
+	array1[4] = 55;
+
+	for (i = 0; i < 5; i++) {
+		printf("%d\n", array1[i]);
+	}
+	printf("Input array position to display value:\n");
+	printf("The value of the array at position %d is %d.\n", 3, array1[3]);
 	return 0;
 }
