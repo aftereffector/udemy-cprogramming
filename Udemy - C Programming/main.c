5a28c1176be58052ac72c26ed8937a5321888c94
Daniel Bates <aftereffector@gmail.com>
2018-01-07 15:36:12 -0500

    Lesson 44: Encrypt with while()

--- Udemy - C Programming/main.c
+++ Udemy - C Programming/main.c
@@ -2,47 +2,55 @@
 
 char plaintext[] = "The quick brown fox jumps over the lazy dog.";
 
-void encryptWithFor() {
-	int i;
+void encryptWithWhile() {
+	int i = 0;
 	char ciphertext[50];
 	char c;
 
 	//Encrypt string
-	for (i = 0; i < 50; i++) {
+	while (i >= 0) {
 		c = plaintext[i];
+		// Continue does not work with while() loops
+		/*
 		if (c == ' ') {
 			ciphertext[i] = c;
 			continue;
 		}
+		*/
 		if (c == '.') {
 			ciphertext[i] = '\0';
 			break;
 		}
 		ciphertext[i] = plaintext[i] + 1;
+		i++;
 	}
 	printf("Encrypted string is '%s'\n", ciphertext);
 
 	//Decrypt string
-	int n;
+	int n = 0;
 	char plaintext2[50];
 	char d;
-	for (n = 0; n < 50; n++) {
+	while (n >= 0) {
 		d = ciphertext[n];
+		// Continue does not work with while() loops
+		/*
 		if (d == ' ') {
 			plaintext2[n] = d;
 			continue;
 		}
+		*/
 		if (d == '\0') {
 			plaintext2[n] = '.';
 			break;
 		}
 		plaintext2[n] = ciphertext[n] - 1;
+		n++;
 	}
 	printf("Decrypted string is '%s'\n", plaintext2);
 }
 
 int main(int argc, char **argv) {
 	printf("Plaintext string is '%s'\n", plaintext);
-	encryptWithFor();
+	encryptWithWhile();
 	return 0;
 }
