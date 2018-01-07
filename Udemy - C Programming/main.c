bc8cd5a98df03ae73d9c08b1348dab23728183be
Daniel Bates <aftereffector@gmail.com>
2018-01-07 15:12:17 -0500

    Lesson 42: Encrypt with for()

--- Udemy - C Programming/main.c
+++ Udemy - C Programming/main.c
@@ -1,18 +1,48 @@
 #include <stdio.h>
 
-int array1[5] = { 1,2,3,4,5 };
-double doublearray[] = { 1.0, 1.1, 1.2, 1.3 };
-char chararray1[] = { 'h', 'e', 'l', 'l', 'o', '\0' };
-char chararray2[] = "world";
+char plaintext[] = "The quick brown fox jumps over the lazy dog.";
+
+void encryptWithFor() {
+	int i;
+	char ciphertext[50];
+	char c;
+
+	//Encrypt string
+	for (i = 0; i < 50; i++) {
+		c = plaintext[i];
+		if (c == ' ') {
+			ciphertext[i] = c;
+			continue;
+		}
+		if (c == '.') {
+			ciphertext[i] = '\0';
+			break;
+		}
+		ciphertext[i] = plaintext[i] + 1;
+	}
+	printf("Encrypted string is '%s'\n", ciphertext);
+
+	//Decrypt string
+	int n;
+	char plaintext2[50];
+	char d;
+	for (n = 0; n < 50; n++) {
+		d = ciphertext[n];
+		if (d == ' ') {
+			plaintext2[n] = d;
+			continue;
+		}
+		if (d == '\0') {
+			plaintext2[n] = '.';
+			break;
+		}
+		plaintext2[n] = ciphertext[n] - 1;
+	}
+	printf("Decrypted string is '%s'\n", plaintext2);
+}
 
 int main(int argc, char **argv) {
-	int i = 0;
-
-	printf("--- do...while loop --- \n");
-	do {
-		printf("%d\n", array1[i]);
-		i++;
-	} while (i < 5);
-
+	printf("Plaintext string is '%s'\n", plaintext);
+	encryptWithFor();
 	return 0;
 }
