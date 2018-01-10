2162f4c096802ed582ca5ec68e6e9b9355016c82
Daniel Bates <aftereffector@gmail.com>
2018-01-09 21:38:42 -0500

    Lesson 54: Structs using strcpy_s

--- Udemy - C Programming/main.c
+++ Udemy - C Programming/main.c
@@ -1,27 +1,41 @@
 #include <stdio.h>
+#include <string.h>
+
+#define NUMBER_OF_CDS 2
+
+struct cd {
+	char name[50];
+	char artist[50];
+	int trackcount;
+	int rating;
+};
+
+struct cd my_cd_collection[NUMBER_OF_CDS];
+
+void create_cdcollection() {
+	strcpy_s(my_cd_collection[0].name, 50, "Wonder What's Next");
+	strcpy_s(my_cd_collection[0].artist, 50, "Chevelle");
+	my_cd_collection[0].trackcount = 9;
+	my_cd_collection[0].rating = 5;
+
+	strcpy_s(my_cd_collection[1].name, 50, "Koi No Yokan");
+	strcpy_s(my_cd_collection[1].artist, 50, "Deftones");
+	my_cd_collection[1].trackcount = 13;
+	my_cd_collection[1].rating = 4;
+}
+
+void display_cdcollection() {
+	int i = 0;
+	struct cd thiscd;
+	while (i < NUMBER_OF_CDS) {
+		thiscd = my_cd_collection[i];
+		printf("CD #%d: '%s' by %s has %d tracks.  My rating is %d/5.\n", i+1, thiscd.name, thiscd.artist, thiscd.trackcount, thiscd.rating);
+		i++;
+	}
+}
 
 int main(int argc, char **argv) {
-
-	char str1[] = "Hello";
-	char *str2 = "Hello";
-	char str3[6];
-
-	str3[0] = 'H';
-	str3[1] = 'e';
-	str3[2] = 'l';
-	str3[3] = 'l';
-	str3[4] = 'o';
-	str3[5] = '\0';
-
-	printf("%x %s\n", str1, str1);
-	printf("%x %s\n", str2, str2);
-	printf("%x %s\n", str3, str3);
-
-	printf("%x, %x, %x, %x, %x, %x\n", &str3[0], &str3[1], &str3[2], &str3[3], &str3[4], &str3[5]);
-	printf("%x, %x, %x, %x, %x, %x\n", str3, str3 + 1, str3 + 2, str3 + 3, str3 + 4, str3 + 5);
-
-	str2 = "Goodbye";
-	printf("str2 - %s\n", str2);
-	
+	create_cdcollection();
+	display_cdcollection();
 	return 0;
 }
