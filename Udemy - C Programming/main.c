edbca196eee54ad5626f35d419325dd8bc2fa96c
Daniel Bates <aftereffector@gmail.com>
2018-01-11 14:49:59 -0500

    Lesson 63-64: File operations
    Added _CRT_SECURE_NO_WARNINGS
    Created test.txt, empty.txt

--- Udemy - C Programming/main.c
+++ Udemy - C Programming/main.c
@@ -1,45 +1,31 @@
 #include <stdio.h>
-#include <string.h>
 
-#define NUMBER_OF_CDS 2
+#define MAXSTRLEN 200
+#define FILENAME "test.txt"
+#define EMPTYFILENAME "empty.txt"
+#define INVALIDFILENAME "invalid.txt"
 
-enum score {
-	Terrible, Bad, Average, Good, Excellent
-};
-
-typedef struct cd {
-	char name[50];
-	char artist[50];
-	int trackcount;
-	enum score rating;
-} CD;
-
-struct cd my_cd_collection[NUMBER_OF_CDS];
-
-void create_cdcollection() {
-	strcpy_s(my_cd_collection[0].name, 50, "Wonder What's Next");
-	strcpy_s(my_cd_collection[0].artist, 50, "Chevelle");
-	my_cd_collection[0].trackcount = 9;
-	my_cd_collection[0].rating = Excellent;
-
-	strcpy_s(my_cd_collection[1].name, 50, "Koi No Yokan");
-	strcpy_s(my_cd_collection[1].artist, 50, "Deftones");
-	my_cd_collection[1].trackcount = 13;
-	my_cd_collection[1].rating = Good;
-}
-
-void display_cdcollection() {
-	int i = 0;
-	struct cd thiscd;
-	while (i < NUMBER_OF_CDS) {
-		thiscd = my_cd_collection[i];
-		printf("CD #%d: '%s' by %s has %d tracks.  My rating is %d/5.\n", i+1, thiscd.name, thiscd.artist, thiscd.trackcount, thiscd.rating);
-		i++;
+void linecount(char *fn) {
+	int numlines = 0;
+	char line[MAXSTRLEN];
+	int *mystreamptr = NULL;
+	FILE *fp;
+	fp = fopen(fn, "r");
+	printf("File pointer for %s is %d\n", fn, fp);
+	if (fp != NULL) {
+		while (fgets(line, sizeof(line), fp) != 0) {
+			numlines++;
+		}
+		fclose(fp);
+		printf("%s contains %d lines of text.\n", fn, numlines);
+	} else {
+		printf("File %s cannot be opened.\n", fn);
 	}
 }
 
 int main(int argc, char **argv) {
-	create_cdcollection();
-	display_cdcollection();
+	linecount(FILENAME);
+	linecount(EMPTYFILENAME);
+	linecount(INVALIDFILENAME);
 	return 0;
 }
